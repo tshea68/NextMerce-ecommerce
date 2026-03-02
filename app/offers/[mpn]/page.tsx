dfd010bf883e9e9cbb240df9bfc3d5da4f131f40
tshea68 <timothyshea1@gmail.com>
2026-03-02 21:12:23 +0000

    Remove app/_components and consolidate all components into components/

--- app/offers/[mpn]/page.tsx
+++ app/offers/[mpn]/page.tsx
@@ -1,7 +1,7 @@
 export const dynamic = "force-dynamic";
 export const runtime = "nodejs";
 
-import ProductPageServer from "@/app/_components/ProductPage.server";
+import ProductPageServer from "@/components/ProductPage.server";
 
 export default async function OffersByMpnPage({
   params,
