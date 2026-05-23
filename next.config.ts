e5a9e56520dbb3992c47155035fa2addbdbea999
tshea68 <timothyshea1@gmail.com>
2026-05-23 16:19:08 +0000

    Use smart legacy refurb redirect route

--- next.config.ts
+++ next.config.ts
@@ -11,11 +11,6 @@ const nextConfig: NextConfig = {
 
   async redirects() {
     return [
-      {
-        source: "/refurb/:mpn",
-        destination: "/offers/:mpn",
-        permanent: true,
-      },
     ];
   },
 };
